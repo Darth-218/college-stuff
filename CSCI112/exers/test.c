e29f8990d23226479346344f6e689b96db868059
Darth218 <64696569+Darth-218@users.noreply.github.com>
2024-03-25 10:55:10 +0200

    a bunch of things

--- CSCI112/exers/test.c
+++ CSCI112/exers/test.c
@@ -1,17 +1,9 @@
 #include <stdio.h>
 
-int IsEven(int a);
-
 int main() {
 
-  int a;
-  scanf("%d", &a);
+  int main = 1;
+  printf("%d", main);
 
-  printf("The number %d is ", a);
-  printf((!(IsEven(a))) ? "even.\n" : "odd.\n");
-}
-
-int IsEven(int a) {
-
-  return (a % 2);
+  }
 }
